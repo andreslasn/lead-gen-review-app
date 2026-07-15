57662a1896b226e83368bafeede2c1914e4e8f6a
Andres Lasn <andres@certific.co>
2026-07-16 01:02:24 +0300

    Fix GitHub Pages blank review app

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   base: "/lead-gen-review-app/",
+  resolve: {
+    alias: {
+      vue: "vue/dist/vue.esm-bundler.js",
+    },
+  },
   plugins: [vue()],
 });
